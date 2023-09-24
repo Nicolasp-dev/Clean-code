97109d666eeba417df230573eaf1e3aaec89ad2a
Nicolas Peña <103202106+Nicolasp-dev@users.noreply.github.com>
2023-09-24 16:19:16 -0500

    Naming Definitions

---
+++ Naming.js
@@ -0,0 +1,42 @@
+// Names should be meaningful
+const user = new User();
+user.save();
+if (isLoggedIn) {
+  //
+}
+
+/*
+  Variables & Constants
+  - Data containers: Use nouns or short phrases with adjectives.
+  - Be descriptive and value type clear.
+  - Name casing conventions: 
+    -  snake_case
+    -  camelCase 
+    -  PascalCase
+    -  kebab-case
+
+  - Booleans may be named as a question response. Ex: isActive
+  - Avoid nonspecific or redundant naming.
+  - Choose Distinctive names
+  - Be consistent.
+
+  Functions & Methods.
+  - Functional operation -> Describe the operation. Ex: getUser()
+  - Functional boolean -> Answer true/false. Ex: isValid()
+
+  Classes 
+  - Describe the object that is created. 
+  - Avoid redundant suffixes
+
+  Exceptions
+  - Getter and Setters
+  - Libraries 
+
+  Errors and Pitfalls
+  - Avoid Redundant information. 
+  - Avoid Slang. Unclear Abbreviations.
+  - Disinformation
+  - 
+
+*/
+
